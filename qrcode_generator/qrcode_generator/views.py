58be9ec74e2b174d351b861566a53048c4d466b5
rajesh <rajesh@gmail.com>
2022-06-16 10:42:39 +0530

    added qr code scanner redirection

--- qrcode_generator/qrcode_generator/views.py
+++ qrcode_generator/qrcode_generator/views.py
@@ -1,18 +1,25 @@
 from django.shortcuts import render
-from qrcode import *
-data = None
+from django.http import HttpResponseRedirect
+# from qrcode import *
+import qrcode
+from MyQR import myqr as mq
 
 
 def home(request):
-    # global data
-
-    # data = 'https://google.com'
-    # img = make(data)
-    # img.save("static/image/test.png")
-    img = make('test text')
-    # <class 'qrcode.image.pil.PilImage'>
-    # (290, 290)
-
-    img.save('static/image/qrcode_test.png')
-
+    data = "http://localhost:8000/redirect"
+    qr = qrcode.QRCode(version = 1,
+                   box_size = 10,
+                   border = 5)
+    qr.add_data(data)
+    qr.make(fit = True)
+    img = qr.make_image(fill_color = 'black',
+                    back_color = 'white')
+    img.save('static/image/test.png')
     return render(request, "home.html", {'data': data})
+
+
+
+def redirect(request):
+    return HttpResponseRedirect("http://google.com")
+
+# 9491105595
